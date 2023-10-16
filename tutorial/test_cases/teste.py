1f83ae0d6b041358aa0152874348c9f17fc86c1b
jose carlos <116109222+esoj03@users.noreply.github.com>
2023-10-16 22:13:55 -0100

    Add files via upload

---
+++ tutorial/test_cases/teste.py
@@ -0,0 +1,29 @@
+""" def soma(a, b):
+    print(f'A soma de {a} e {b} foi {a + b}')
+
+
+soma(2,4) """
+
+#fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75, 'limes': 0.75, 'strawberries': 1.00}
+
+#lista = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
+
+#soma = 0.0
+#for chave, valor in fruitPrices:
+
+
+
+       # else:
+       #     print("Fruta nao encontrada!!!")
+
+'''def calcular_custo_lista_de_tuplas(lista_de_tuplas):
+    custo_total = 0
+    for tupla in lista_de_tuplas:
+        custo_tupla = sum(tupla)
+        custo_total += custo_tupla
+    return custo_total
+
+lista_de_tuplas = [(1, 2), (3, 4), (5, 6)]
+custo = calcular_custo_lista_de_tuplas(lista_de_tuplas)
+print("Custo total da lista de tuplas:", custo)'''
+
